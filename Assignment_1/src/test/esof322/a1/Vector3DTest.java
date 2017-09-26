f428ccb8eeafcf596b96abc1877ba378ad0fdf1f
Brick7Face <Brick7Face@users.noreply.github.com>
2017-09-26 14:09:08 -0600

    adds add test and method

--- Assignment_1/src/test/esof322/a1/Vector3DTest.java
+++ Assignment_1/src/test/esof322/a1/Vector3DTest.java
@@ -10,6 +10,13 @@ public class Vector3DTest {
 
 
 	@Test
+	public void testAdd() {
+		Vector3D v = new Vector3D(1, 2, 3);
+		Vector3D sum = new Vector3D(3, 6, 9);
+		assertTrue(sum.equals(v.add(new Vector3D(2, 4, 6))));
+	}
+	
+	@Test
 	public void testNegate() {
 		Vector3D v = new Vector3D(1, 2, 3);
 		Vector3D neg = new Vector3D(-1, -2, -3);
