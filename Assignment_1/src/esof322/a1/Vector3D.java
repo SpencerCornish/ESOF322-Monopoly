a6ca8fa158fc9e145cf16f525c264af1dbd6f136
SpencerCornish <spencerjcornish@gmail.com>
2017-09-28 15:15:35 -0600

    Format and fix

--- Assignment_1/src/esof322/a1/Vector3D.java
+++ Assignment_1/src/esof322/a1/Vector3D.java
@@ -11,57 +11,54 @@ public class Vector3D {
 		this.zCoord = zCoord;
 	}
 
-	//Adds vector by returning a new vector with the coordinates of the parameter plus the this instance, produces new object
+	// Adds vector by returning a new vector with the coordinates of the parameter
+	// plus the this instance, produces new object
 	public Vector3D add(Vector3D v) {
 		return new Vector3D((xCoord + v.xCoord), (yCoord + v.yCoord), (zCoord + v.zCoord));
 	}
-	
-	/// Negates the vector by returning a new vector with coordinates negated, produces new object
+
+	/// Negates the vector by returning a new vector with coordinates negated,
+	/// produces new object
 	public Vector3D negate() {
 		return new Vector3D(-xCoord, -yCoord, -zCoord);
 	}
 
-	//Subtracts v's coordinates from coordinates in this, produces new object
-	public Vector3D subtract(Vector3D v){
+	// Subtracts v's coordinates from coordinates in this, produces new object
+	public Vector3D subtract(Vector3D v) {
 		return new Vector3D((xCoord - v.xCoord), (yCoord - v.yCoord), (zCoord - v.zCoord));
 	}
 
-	//Multiply each coordinate by value f, produce new object
+	// Multiply each coordinate by value f, produce new object
 	public Vector3D scale(double f) {
 		return new Vector3D((f * xCoord), (f * yCoord), (f * zCoord));
 	}
 
-	//Produces dot product of this and argument v, returns a double
-	public double dot(Vector3D v){
-		return this.xCoord*v.xCoord + this.yCoord*v.yCoord + this.zCoord*v.zCoord;
+	// Produces dot product of this and argument v, returns a double
+	public double dot(Vector3D v) {
+		return this.xCoord * v.xCoord + this.yCoord * v.yCoord + this.zCoord * v.zCoord;
 	}
-	
-	//Calculates the magnitude of vector3D
+
+	// Calculates the magnitude of vector3D
 	public double magnitude() {
-		return (Math.sqrt(xCoord*xCoord + yCoord*yCoord + zCoord*zCoord));
+		return (Math.sqrt(xCoord * xCoord + yCoord * yCoord + zCoord * zCoord));
 	}
-	
+
 	@Override
-	public String toString() {	
-		return ("The X coordinate is: " + xCoord + " The Y coordinate is: " + yCoord 
-				+ " The Z coordinate is: " + zCoord);
+	public String toString() {
+		return ("The X coordinate is: " + xCoord + " The Y coordinate is: " + yCoord + " The Z coordinate is: "
+				+ zCoord);
 	}
-	
-	
-	
-	
-	
+
 	@Override
 	public boolean equals(Object o) {
-		if (o instanceof Vector3D) {
-			double precision = 0.001;
-			double differenceX = Math.abs(((Vector3D) o).xCoord - this.xCoord);
-			double differenceY = Math.abs(((Vector3D) o).yCoord - this.yCoord);
-			double differenceZ = Math.abs(((Vector3D) o).zCoord - this.zCoord);
-			if (differenceX < precision && differenceY < precision && differenceZ < precision ) {
-				return true;
-			}
+		double precision = 0.001;
+		double differenceX = Math.abs(((Vector3D) o).xCoord - this.xCoord);
+		double differenceY = Math.abs(((Vector3D) o).yCoord - this.yCoord);
+		double differenceZ = Math.abs(((Vector3D) o).zCoord - this.zCoord);
+		if (differenceX < precision && differenceY < precision && differenceZ < precision) {
+			return true;
 		}
+
 		return false;
 	}
 }
