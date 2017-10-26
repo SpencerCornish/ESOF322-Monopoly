6f7dc10f52af9cc877cddd30b74505c8d7630e6d
Bryan Plant <bryanplant@gmail.com>
2017-10-26 13:07:06 -0600

    Adds basic canvas functionality

--- Monopoly/lib/src/components/app/app.dart
+++ Monopoly/lib/src/components/app/app.dart
@@ -1,10 +1,18 @@
 import 'dart:html';
 import 'dart:async';
 
+CanvasElement canvas = querySelector("#canvas");
+CanvasRenderingContext2D ctx = canvas.getContext('2d');
+
 void main() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
   Timer tempLoadingTimeout = new Timer(new Duration(seconds: 4), _changeStuff);
 }
 
 void _changeStuff() {
   querySelector('#output').text = 'Your Dart app is running.';
+  ctx.fillStyle = 'red';
+  ctx.fillRect(100, 100, 250, 250);
 }
