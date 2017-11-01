2fea89135e7d8e94cc9bac955254ea9c5c31c4e4
Bryan Plant <bryanplant@gmail.com>
2017-11-01 10:07:41 -0600

    board and tiles resize on window resize

--- Monopoly/lib/src/components/board/board.dart
+++ Monopoly/lib/src/components/board/board.dart
@@ -4,24 +4,43 @@ import '../tiles/tile.dart';
 class Board {
   int x;
   int y;
-  int size;
+  int tileSize;
   List<Tile> tiles;
 
   Board() {
     tiles = new List<Tile>();
-    size = 75;
-    x = (window.innerWidth / 2 - size * 5.5).toInt();
-    y = (window.innerHeight / 2 - size * 5.5).toInt();
+    if(window.innerWidth > window.innerHeight)
+      tileSize = ((window.innerHeight-50)/11).toInt();
+    else
+      tileSize = ((window.innerWidth-50)/11).toInt();
+    x = (window.innerWidth / 2 - tileSize * 5.5).toInt();
+    y = (window.innerHeight / 2 - tileSize * 5.5).toInt();
     for (int i = 0; i < 10; i++) {
-      tiles.add(new Tile("name", x + i * size, y, size));
-      tiles.add(new Tile("name", x, y + (i+1) * size, size));
-      tiles.add(new Tile("name", x + (i+1) * size, 10 * size + y, size));
-      tiles.add(new Tile("name", 10 * size + x, y + i * size, size));
+      tiles.add(new Tile("name", x + i * tileSize, y, tileSize));
+      tiles.add(new Tile("name", x, y + (i+1) * tileSize, tileSize));
+      tiles.add(new Tile("name", x + (i+1) * tileSize, 10 * tileSize + y, tileSize));
+      tiles.add(new Tile("name", 10 * tileSize + x, y + i * tileSize, tileSize));
     }
   }
 
   void draw(CanvasRenderingContext2D ctx) {
     for(Tile tile in tiles)
       tile.draw(ctx);
+  }
+
+  void resize(){
+    if(window.innerWidth > window.innerHeight)
+      tileSize = ((window.innerHeight-50)/11).toInt();
+    else
+      tileSize = ((window.innerWidth-50)/11).toInt();
+    x = (window.innerWidth / 2 - tileSize * 5.5).toInt();
+    y = (window.innerHeight / 2 - tileSize * 5.5).toInt();
+    tiles = new List<Tile>();
+    for (int i = 0; i < 10; i++) {
+      tiles.add(new Tile("name", x + i * tileSize, y, tileSize));
+      tiles.add(new Tile("name", x, y + (i+1) * tileSize, tileSize));
+      tiles.add(new Tile("name", x + (i+1) * tileSize, 10 * tileSize + y, tileSize));
+      tiles.add(new Tile("name", 10 * tileSize + x, y + i * tileSize, tileSize));
+    }
   }
 }
