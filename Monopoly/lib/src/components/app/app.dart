342c4af15335b2000461cc1f699054cd9084cdf8
Spencer Cornish <spencerjcornish@gmail.com>
2017-11-07 22:12:46 -0700

    Adds all the buttons

--- Monopoly/lib/src/components/app/app.dart
+++ Monopoly/lib/src/components/app/app.dart
@@ -20,20 +20,27 @@ class App {
 
   List<HtmlElement> _buttons = new List<HtmlElement>();
 
-  ButtonElement _rollDiceButton;
-  ButtonElement _buyPropertyButton;
+  LabelElement _statusLabel;
+
+  // Top button control refs
+  LinkElement _rollDiceButton;
+  LinkElement _buyPropertyButton;
+  LinkElement _auctionPropertyButton;
+  LinkElement _endTurnButton;
+
+  // Bottom button control refs
+  LinkElement _mortgagePropertyButton;
+  LinkElement _buildBuildingButton;
+  LinkElement _buyBuildingButton;
+  LinkElement _sellBuildingButton;
 
   Board _board = new Board();
 
   App() {
-    _rollDiceButton = new ButtonElement();
-    _rollDiceButton.text = 'Roll Dice';
-    _buttons.add(_rollDiceButton);
-
-    _buyPropertyButton = new ButtonElement();
-    _buyPropertyButton.text = 'Buy Property';
-    _buttons.add(_buyPropertyButton);
-
+    _constructButtonControls();
+    // Instantiate a board
+    // Board should parse CSV file for tile data and create tiles
+    // Board should hold a list of players in jail?
     _canvasBackground = querySelector("#canvas-background");
     _ctxBackground = _canvasBackground.getContext('2d');
 
@@ -47,15 +54,8 @@ class App {
     _canvasForeground.width = window.innerWidth ?? 1024;
     _canvasForeground.height = window.innerHeight ?? 768;
 
-    // Instantiate a board
-    // Board should parse CSV file for tile data and create tiles
-    // Board should hold a list of players in jail?
-
     // Instantiate players
     // Player should instantiate game_pieces/banks
-
-    _rollDiceButton.onClick.listen((e) => print('roll the dice'));
-    _buyPropertyButton.onClick.listen((e) => print('buy the property'));
 
     window.onResize.listen((e) {
       _canvasBackground.width = window.innerWidth;
@@ -67,16 +67,14 @@ class App {
       _beginDraw();
     });
 
-    Timer loadingSplashScreenTimeout =
-        new Timer(new Duration(seconds: 1), _beginDraw);
+    Timer loadingSplashScreenTimeout = new Timer(new Duration(seconds: 1), _beginDraw);
   }
 
   _beginDraw() {
     _ctxBackground.clearRect(0, 0, window.innerWidth, window.innerHeight);
     _ctxForeground.clearRect(0, 0, window.innerWidth, window.innerHeight);
     querySelector('#output').text = '';
-    for(ButtonElement button in _buttons)
-      querySelector('#buttons').children.add(button);
+    for (ButtonElement button in _buttons) querySelector('.top-button-container').children.add(button);
     _board.draw(_ctxBackground);
   }
 
@@ -90,5 +88,65 @@ class App {
     }
   }
 
+  _constructButtonControls() {
+    _statusLabel = new LabelElement();
+    _statusLabel.text = ':)';
+    _statusLabel.className = 'is-unselectable';
+    _buttons.add(_statusLabel);
+
+    _rollDiceButton = new LinkElement();
+    _rollDiceButton.text = 'Roll Dice';
+    _rollDiceButton.classes = _constructButtonClasses('is-info');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_rollDiceButton);
+
+    _buyPropertyButton = new LinkElement();
+    _buyPropertyButton.text = 'Buy Tile';
+    _buyPropertyButton.classes = _constructButtonClasses('is-static');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_buyPropertyButton);
+
+    _auctionPropertyButton = new LinkElement();
+    _auctionPropertyButton.text = 'Auction Tile';
+    _auctionPropertyButton.classes = _constructButtonClasses('is-static');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_auctionPropertyButton);
+
+    _endTurnButton = new LinkElement();
+    _endTurnButton.text = 'End Turn';
+    _endTurnButton.classes = _constructButtonClasses('is-danger');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_endTurnButton);
+
+    _mortgagePropertyButton = new LinkElement();
+    _mortgagePropertyButton.text = 'Mortgage Tiles';
+    _rollDiceButton.classes = _constructButtonClasses('is-info');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_mortgagePropertyButton);
+
+    _buyBuildingButton = new LinkElement();
+    _buyBuildingButton.text = 'Buy Buildings';
+    _buyBuildingButton.classes = _constructButtonClasses('is-info');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_buyBuildingButton);
+
+    _sellBuildingButton = new LinkElement();
+    _sellBuildingButton.text = 'Sell Buildings';
+    _sellBuildingButton.classes = _constructButtonClasses('is-static');
+    _rollDiceButton.onClick(_rollDice);
+    _buttons.add(_sellBuildingButton);
+  }
+
+  _constructButtonClasses(String extraClasses, [String extraClassTwo = "a"]) => [
+        'button',
+        'is-success',
+        'padded',
+        'is-centered',
+        'is-small',
+        extraClasses,
+        extraClassTwo,
+      ];
   _buildBoard(String boardCsv) {}
+
+  _rollDice() {}
 }
