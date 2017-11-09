7857fd94a9686e9626ed38af06edad95eb63383e
Nate Tranel <njtranel@gmail.com>
2017-11-08 20:20:53 -0700

    adds a board test

--- Monopoly/test/unit/src/components/board/board_test.dart
+++ Monopoly/test/unit/src/components/board/board_test.dart
@@ -2,13 +2,16 @@ import "package:test/test.dart";
 import "package:monopoly/src/components/board/board.dart";
 
 void main() {
+  Board testBoard;
   setUp(() {
-    Board testBoard = new Board();
+    testBoard = new Board();
   });
   group("Board", () {
-    test("some asyncronous test", () {
-      bool someBool = true;
-      expect(someBool, isTrue);
+    test("returns true if readInfo() reads correctly", () {
+      List<String> list = testBoard.readInfo();
+      expect(list.elementAt(0), "Go");
+      expect(list.elementAt(7), "0");
+      expect(list.elementAt(14), "Street");
     });
   });
 }
